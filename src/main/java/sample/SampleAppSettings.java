eff2dd3675f0bf86c91b8a8df6a3909eda455718
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-11-13 12:53:28 +0900

    プロパティ・ファイルによる設定にデフォルト値を用意

--- src/main/java/sample/SampleAppSettings.java
+++ src/main/java/sample/SampleAppSettings.java
@@ -4,20 +4,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * アプリケーションの設定情報を保持するオブジェクト.
+ * <p>プロパティ・ファイルで定義された設定情報をインスタンス・フィールドに保持する。</p>
+ */
 @Configuration
 @PropertySource("classpath:application.properties")
 public class SampleAppSettings {
-	@Value("${sample.app.name}")
+	@Value("${sample.app.name:Sample.app}")
 	private String appName;
-	@Value("${sample.app.version}")
+	@Value("${sample.app.version:1.0-BETA}")
 	private String appVersion;
-	@Value("${sample.xsrf.protection.enabled}")
+	@Value("${sample.xsrf.protection.enabled:true}")
 	private boolean xsrfProtectionEnabled;
-	@Value("${sample.xsrf.protection.cookie.name}")
+	@Value("${sample.xsrf.protection.cookie.name:XSRF-TOKEN}")
 	private String xsrfProtectionCookieName;
-	@Value("${sample.xsrf.protection.header.name}")
+	@Value("${sample.xsrf.protection.header.name:X-XSRF-TOKEN}")
 	private String xsrfProtectionHeaderName;
-	@Value("${sample.access.control.enabled}")
+	@Value("${sample.access.control.enabled:true}")
 	private boolean accessControlEnabled;
 	
 	public boolean isXsrfProtectionEnabled() {
