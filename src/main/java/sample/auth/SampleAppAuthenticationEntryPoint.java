314a488cbb108a425ed0a5d0b4b12c6aaf9ce654
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-11-13 12:55:06 +0900

    独自・特殊実装となっていたステータスコード401のときのレスポンス生成処理を共通処理に移譲

--- src/main/java/sample/auth/SampleAppAuthenticationEntryPoint.java
+++ src/main/java/sample/auth/SampleAppAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package sample.auth;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,27 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 /**
  * サンプル・アプリケーションのための認証エントリーポイント.
  * <p>Spring Bootのデフォルトでは認証失敗時にHTTPステータスコード403を返すエントリーポイントが使用される。
  * それでも良いといえば良いのだが、「認証されていない（だから認証手続きをすべきだ）」という旨を表現したいため、
  * 独自のエントリーポイント実装を提供する。</p>
- * @return 認証エントリーポイント
  */
 public class SampleAppAuthenticationEntryPoint implements AuthenticationEntryPoint {
-	private final ObjectMapper mapper = new ObjectMapper();
-	
 	@Override
 	public void commence(
 			final HttpServletRequest req, 
 			final HttpServletResponse resp, 
 			final AuthenticationException ex)
 					throws IOException, ServletException {
-		resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		final PrintWriter pw = resp.getWriter();
-		mapper.writeValue(pw, ex);
-		pw.flush();
+		resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, ex.getMessage());
 	}
 }
