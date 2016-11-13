0bcb7be637b5e383dcd43f61fa88a5957a2ddf53
mizuky fujitani <mizuki.fujitani@gmail.com>
2016-11-13 12:53:54 +0900

    Javadocを修正

--- src/main/java/sample/SampleAppServletInitializer.java
+++ src/main/java/sample/SampleAppServletInitializer.java
@@ -6,9 +6,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * ServletContextを構成するためのインターフェース.
  * <p>web.xmlの代わりにJavaプログラムによりServletContextを構成する。</p>
- * <p>サンプルのアプリはwarファイルをビルドしスタンドアロンのサーブレット・コンテナに
- * デプロイするタイプのアプリケーションなので、
- * このインターフェースを使って設定を変更する必要がある。</p>
+ * <p>サンプル・アプリケーションはwarファイルをビルドしスタンドアロンのサーブレット・コンテナに
+ * デプロイする想定で構築しているので、このインターフェースを使って設定を変更する必要がある。</p>
  */
 public class SampleAppServletInitializer extends SpringBootServletInitializer {
 	@Override
